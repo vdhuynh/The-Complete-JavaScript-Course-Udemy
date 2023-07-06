6847e659edcba03d6648ef1021d62bdab6833c8b
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-06 18:25:19 -0500

    Create challenge_2.js

---
+++ challenge_2.js
@@ -0,0 +1,13 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+BMIMark > BMIJohn
+  ? console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+  : console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
