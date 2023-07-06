c21e048a84c573696b212f10b6904767546ad9d9
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-06 18:49:15 -0500

    Create challenge_4.js

---
+++ challenge_4.js
@@ -0,0 +1,6 @@
+const bill = 275;
+
+const tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+const totalValue = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`);
